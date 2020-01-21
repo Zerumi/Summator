e92b2fab46fdd84183c5e17d9cba33a264e8efb6
Zerumi <46845708+Zerumi@users.noreply.github.com>
2020-01-21 16:30:26 +0400

    Add project files, .gitignore, .gitattributes

---
+++ WpfApp1/WpfApp1/Calculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    static class Calculator
+    {
+        public enum Symbols
+        {
+            Sum = '+',
+            Dif = '-',
+            Mul = '*',
+            Div = '/'
+        }
+
+        public static void DeleteZeroIfExists (MainWindow window)
+        {
+            if (window.tbOutput.Text == "0" || window.isNextNumStartWrite)
+            {
+                window.tbOutput.Text = string.Empty;
+                window.isNextNumStartWrite = false;
+            }
+        }
+        static int FirstNum { get; set; }
+        static int SecondNum { get; set; }
+        static bool IsFirstNumAdded = false;
+        public static void AddNumber(int number)
+        {
+            if (IsFirstNumAdded)
+            {
+                SecondNum = number;
+            }
+            else
+            {
+                FirstNum = number;
+            }
+            IsFirstNumAdded = !IsFirstNumAdded;
+        }
+        public static string Calculate(Symbols symbol)
+        {
+            try
+            {
+                if (!IsFirstNumAdded)
+                {
+                    switch (symbol)
+                    {
+                        case Symbols.Sum:
+                            return Convert.ToString(FirstNum + SecondNum);
+                        case Symbols.Dif:
+                            return Convert.ToString(FirstNum - SecondNum);
+                        case Symbols.Mul:
+                            return Convert.ToString(FirstNum * SecondNum);
+                        case Symbols.Div:
+                            return Convert.ToString(FirstNum / SecondNum);
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Недостаточно параметров");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                IsFirstNumAdded = false;
+            }
+            return "0";
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator.Symbols symbol = new Calculator.Symbols();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool isNextNumStartWrite = false;
+
+        private void BClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = "0";
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "1";
+        }
+
+        private void B2_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "2";
+        }
+
+        private void B3_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "3";
+        }
+
+        private void B4_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "4";
+        }
+
+        private void B5_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "5";
+        }
+
+        private void B6_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "6";
+        }
+
+        private void B7_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "7";
+        }
+
+        private void B8_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "8";
+        }
+
+        private void B9_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "9";
+        }
+
+        private void B0_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.DeleteZeroIfExists(this);
+            tbOutput.Text += "0";
+        }
+
+        private void BDiv_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.AddNumber(Convert.ToInt32(tbOutput.Text));
+            symbol = Calculator.Symbols.Div;
+            isNextNumStartWrite = true;
+        }
+
+        private void BMult_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.AddNumber(Convert.ToInt32(tbOutput.Text));
+            symbol = Calculator.Symbols.Mul;
+            isNextNumStartWrite = true;
+        }
+
+        private void BDif_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.AddNumber(Convert.ToInt32(tbOutput.Text));
+            symbol = Calculator.Symbols.Dif;
+            isNextNumStartWrite = true;
+        }
+
+        private void BSum_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.AddNumber(Convert.ToInt32(tbOutput.Text));
+            symbol = Calculator.Symbols.Sum;
+            isNextNumStartWrite = true;
+        }
+
+        private void BCalc_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator.AddNumber(Convert.ToInt32(tbOutput.Text));
+            tbOutput.Text = Calculator.Calculate(symbol);
+            isNextNumStartWrite = true;
+        }
+    }
+}
